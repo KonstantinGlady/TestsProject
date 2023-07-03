c4fa6e2362ee57a98d777c07cc82e6688ac2ce26
KonstantinGlady <gladyi@hotmail.com>
2023-07-03 16:13:31 +0200

    allScoreForLetter red

--- TDDWithJava/src/test/java/com/wordz/domain/WordTest.java
+++ TDDWithJava/src/test/java/com/wordz/domain/WordTest.java
@@ -28,6 +28,15 @@ class WordTest {
         assertScoreForLetter(score, 1, Letter.PART_CORRECT);
     }
 
+    @Test
+    void allScoreCombination() {
+        var word = new Word("ARI");
+        var score = word.guess("ZAI");
+        assertScoreForLetter(score, 0, Letter.INCORRECT);
+        assertScoreForLetter(score, 1, Letter.PART_CORRECT);
+        assertScoreForLetter(score, 2, Letter.CORRECT);
+    }
+
     private void assertScoreForLetter(Score score, int position, Letter expected) {
         assertThat(score.letter(position)).isEqualTo(expected);
     }
