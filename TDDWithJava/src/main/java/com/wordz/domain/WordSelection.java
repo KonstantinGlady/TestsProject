221f32da3cc9ad914364aab9ed5ecb10c9c85b2b
KonstantinGlady <gladyi@hotmail.com>
2023-07-05 17:50:36 +0200

    WordSelectionTest green

--- TDDWithJava/src/main/java/com/wordz/domain/WordSelection.java
+++ TDDWithJava/src/main/java/com/wordz/domain/WordSelection.java
@@ -1,13 +1,19 @@
 package com.wordz.domain;
 
 public class WordSelection {
+    private final WordRepository repository;
+    private final RandomNumber random;
+
     public WordSelection(WordRepository repository, RandomNumber random) {
+        this.repository = repository;
+        this.random = random;
     }
 
     public String chooseRandomWord() {
 
         try {
-           return " ";
+            int wordNumber = random.next(repository.highestWordNumber());
+            return repository.fetchWordByNumber(wordNumber);
         } catch (WordRepositoryException ex) {
             throw new WordSelectionException("Could not select word", ex);
         }
